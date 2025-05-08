4848d0fe8b76bb0c924e59f068e1d1f4d9c2d8b8
samuel <samuel.kuhn@gmx.ch>
2025-05-08 23:04:52 +0200

    strip down

--- src/main/java/com/minecraft/forge/veinmining/VeinMiningMod.java
+++ src/main/java/com/minecraft/forge/veinmining/VeinMiningMod.java
@@ -1,15 +1,8 @@
 package com.minecraft.forge.veinmining;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(VeinMiningMod.MOD_ID)
@@ -22,26 +15,6 @@ public class VeinMiningMod {
     }
 
     public VeinMiningMod() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event) {    }
-
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {    }
-
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-
-        }
     }
 }
